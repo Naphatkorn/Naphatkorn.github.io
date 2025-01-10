c2382c75b711727ebb8b1aeb556cc0b7e1cfe35e
Naphatkorn <96692483+Naphatkorn@users.noreply.github.com>
2025-01-10 09:57:35 +0700

    10/1/2568

--- danger/danger.js
+++ danger/danger.js
@@ -1,14 +1,4 @@
-let acc = document.getElementsByClassName("accordion");
-for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", 
-    function() {
-        this.classList.toggle("active");
-        let panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    }
-);
+let mem = document.getElementById("member-click");
+function mem_clicked() {
+    
 }
